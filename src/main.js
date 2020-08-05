df2fbfbbcfcb249988b0e80c3f9b2803efc46c25
Do Minh Khai <khaihkd@gmail.com>
2020-08-05 15:04:10 +0700

    remove console.log

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-console.log('kkkkk', process.env)
+
 Vue.use(VueAnalytics, {
   id: process.env.VUE_APP_GA_ID,
   router
